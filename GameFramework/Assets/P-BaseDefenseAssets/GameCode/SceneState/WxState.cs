fb03738f89d56a6a44926a24df64f7b827a5647f
HNGZY <gzy1115wyyx@163.com>
2021-05-14 15:26:02 +0800

    GameFramework
    基于各种设计模式的游戏框架Demo

---
+++ GameFramework/Assets/P-BaseDefenseAssets/GameCode/SceneState/WxState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WxState : ISceneState
+{
+	public WxState(SceneStateController Controller) : base(Controller)
+	{
+		this.StateName = "Wx";
+	}
+
+	// 開始
+	public override void StateBegin()
+	{
+		// 取得開始按鈕
+		Button tmpBtn = UITool.GetUIComponent<Button>("WXButton");
+		if (tmpBtn != null)
+			tmpBtn.onClick.AddListener(() => OnStartGameBtnClick(tmpBtn)) ;
+	}
+
+	// 開始戰鬥
+	private void OnStartGameBtnClick(Button theButton)
+	{
+		//Debug.Log ("OnStartBtnClick:"+theButton.gameObject.name);
+		m_Controller.SetState(new BattleState(m_Controller), "BattleScene");
+	}
+}
